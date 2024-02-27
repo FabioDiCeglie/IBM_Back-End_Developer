d1e3d74dea23f7ad5039ea9c54b27344db565671
FabioDiCeglie <fabiod@backbase.com>
2024-02-27 16:07:58 +0100

    fix: pylint errors

--- Python AI Application/project_ai/SentimentAnalysis/sentiment_analysis.py
+++ Python AI Application/project_ai/SentimentAnalysis/sentiment_analysis.py
@@ -1,22 +1,33 @@
-import requests
+"""
+Module for sentiment analysis.
+"""
+
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
-   url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-   myobj = { "raw_document": { "text": text_to_analyse } }
-   header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-   try:
-      response = requests.post(url, json = myobj, headers=header)
-      formatted_response = json.loads(response.text)
-      label = formatted_response['documentSentiment']['label']
-      score = formatted_response['documentSentiment']['score']
-      if response.status_code == 200:
-        label = formatted_response['documentSentiment']['label']
-        score = formatted_response['documentSentiment']['score']
-      elif response.status_code == 500:
-        label = None
-        score = None
-      return {'label': label, 'score': score}
-   except requests.exceptions.RequestException as e:
-       print("Error: ", e)
-       return None
+    """
+    Analyze sentiment of the given text.
+
+    Args:
+        text_to_analyse (str): The text to analyze.
+
+    Returns:
+        dict: Containing 'label' and 'score' of the sentiment analysis.
+    """
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    try:
+        response = requests.post(url, json=myobj, headers=header, timeout=10)
+        formatted_response = json.loads(response.text)
+        if response.status_code == 200:
+            label = formatted_response['documentSentiment']['label']
+            score = formatted_response['documentSentiment']['score']
+        elif response.status_code == 500:
+            label = None
+            score = None
+        return {'label': label, 'score': score}
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
