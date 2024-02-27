45f6623583527fa44978a1c2c6b7abf9cf0918fa
FabioDiCeglie <fabiod@backbase.com>
2024-02-27 16:05:07 +0100

    fix: pylint errors

--- Python AI Application/project_ai/server.py
+++ Python AI Application/project_ai/server.py
@@ -9,18 +9,30 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    """
+    Analyzes the sentiment of the provided text.
+    Parameters:
+        text_to_analyze (str): The text to be analyzed for sentiment.
+    Returns:
+        str: A message indicating the sentiment of the provided text and its score.
+             Returns an error message if the input is invalid.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
     if label is None:
         return "Invalid input ! Try again."
-    else:
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index page of the web application.
+
+    Returns:
+        str: Rendered HTML content for the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
