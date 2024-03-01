0d4c1a551f3b01a18867d5a0d02f09cab93496f7
FabioDiCeglie <fabiod@backbase.com>
2024-03-01 10:47:55 +0100

    feat: created ORM from 0

---
+++ Django-SQL-Applications/ormtemplate/settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'uhvgrugi',
+        'USER': 'uhvgrugi',
+        'PASSWORD': 'ym3OVFcclFJ7VZ9NZYGgRXc4AmqYAYR3',
+        'HOST': 'cornelius.db.elephantsql.com',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
