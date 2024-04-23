9ff04ea35d76eaf3219455342725916de04b24a0
FabioDiCeglie <fabiod@backbase.com>
2024-04-23 11:16:30 +0200

    feat: completed all the routes

--- FinalProject/server-songs/backend/routes.py
+++ FinalProject/server-songs/backend/routes.py
@@ -85,16 +85,27 @@ def create_song():
     db.songs.insert_one(song_in)
     return {"inserted song": parse_json(song_in)}, 201
 
-@app.route("/song", methods=["PUT"])
-def create_song():
+@app.route("/song/<int:id>", methods=["PUT"])
+def update_song(id):
     song_in = request.json
-    
-    # if the id is already there, return 303 with the URL for the resource
-    song = db.songs.find_one({"id": song_in["id"]})
-    if song:
-        return {
-            "Message": f"song with id {song_in['id']} already present"
-        }, 302
 
-    db.songs.insert_one(song_in)
-    return {"inserted song": parse_json(song_in)}, 201
+    song = db.songs.find_one({"id": id})
+    if song == None:
+        return {"message": "song not found"}, 404
+    
+    updated_data = {"$set": song_in}
+    result = db.songs.update_one({"id": id}, updated_data)
+    if result.modified_count == 0:
+        return {"message": "song found, but nothing updated"}, 200
+    else:
+        return parse_json(db.songs.find_one({"id": id})), 201
+    
+@app.route("/song/<int:id>", methods=["DELETE"])
+def delete_song(id):
+
+    result = db.songs.delete_one({"id": id})
+
+    if result.deleted_count == 0:
+        return {"message": "song not found"}, 404
+    else:
+        return "", 204
