51549b676d0dfeafa3f65de354609e699cba8632
FabioDiCeglie <fabiod@backbase.com>
2024-04-24 08:49:42 +0200

    fix: login and logout view

--- FinalProject/server-django/concert/views.py
+++ FinalProject/server-django/concert/views.py
@@ -47,10 +47,22 @@ def photos(request):
     return render(request, "photos.html", {"photos": photos})
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+
+            if user.check_password(password):
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "login.html", {"form": LoginForm})
     return render(request, "login.html", {"form": LoginForm})
 
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
 
 def concerts(request):
     pass
